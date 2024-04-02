00761c49fd432b35c314ecf0b8b6e5067d99bb58
fedor <f.garanin@itspecial.net>
2024-04-03 00:44:01 +0400

    Hotfix.1

--- components/common/Scope.vue
+++ components/common/Scope.vue
@@ -1,5 +1,21 @@
 <template>
   <section class="app-common-scope">
-    <nuxt-loading-indicator :throttle="0" :duration="5e3" />
+    <nuxt-loading-indicator
+      :throttle="0"
+      :duration="5e3"
+      :color="indicatorColor"
+    />
   </section>
 </template>
+
+<script lang="ts" setup>
+import { useThemeProvider } from '~/composables/useThemeProvider'
+
+const themeProvider = useThemeProvider()
+const indicatorColor = computed(() => {
+  if (themeProvider.getCurrentTheme.value.currentTheme === 'dark') {
+    return 'white'
+  }
+  return 'black'
+})
+</script>
